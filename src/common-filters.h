aefbddd3dda6e1f28f4c32a7bc2077a6145d53e6
bourdaud <bourdaud@3b4714c8-b19d-487d-9711-26c957be2895>
2009-08-21 13:57:10 +0000

    Make header file C++ compatible and add license notices
    git-svn-id: https://svn.epfl.ch/svn/activetwotools/trunk/preprocessing/rtfilter@120 3b4714c8-b19d-487d-9711-26c957be2895

--- src/common-filters.h
+++ src/common-filters.h
@@ -1,5 +1,26 @@
+/*
+      Copyright (C) 2008-2009 Nicolas Bourdaud <nicolas.bourdaud@epfl.ch>
+
+    This file is part of the rtfilter library
+
+    The eegpanel library is free software: you can redistribute it and/or
+    modify it under the terms of the version 3 of the GNU General Public
+    License as published by the Free Software Foundation.
+  
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #ifndef COMMON_FILTERS_H
 #define COMMON_FILTERS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 #include <filter.h>
 
@@ -9,12 +30,28 @@ typedef enum {
 	RECT_WINDOW
 } KernelWindow;
 
-hfilter create_fir_filter_mean(unsigned int nsamples, unsigned int nchann, unsigned int type);
-hfilter create_fir_filter_lowpass(double fc, unsigned int half_length, unsigned int nchann, KernelWindow window, unsigned int type);
-hfilter create_fir_filter_highpass(double fc, unsigned int half_length, unsigned int nchann, KernelWindow window, unsigned int type);
-hfilter create_fir_filter_bandpass(double fc_low, double fc_high, unsigned int half_length, unsigned int nchann, KernelWindow window, unsigned int type);
-hfilter create_butterworth_filter(double fc, unsigned int num_pole, unsigned int num_chann, int highpass, unsigned int type);
-hfilter create_chebychev_filter(double fc, unsigned int num_pole, unsigned int nchann, int highpass, double ripple, unsigned int type);
+hfilter create_fir_filter_mean(unsigned int nsamples, unsigned int nchann,
+                               unsigned int type);
+hfilter create_fir_filter_lowpass(double fc, unsigned int half_length,
+                                  unsigned int nchann, KernelWindow window,
+				  unsigned int type);
+hfilter create_fir_filter_highpass(double fc, unsigned int half_length,
+                                   unsigned int nchann, KernelWindow window,
+				   unsigned int type);
+hfilter create_fir_filter_bandpass(double fc_low, double fc_high,
+                                   unsigned int half_length,
+				   unsigned int nchann, KernelWindow window,
+				   unsigned int type);
+hfilter create_butterworth_filter(double fc, unsigned int num_pole,
+                                  unsigned int num_chann, int highpass,
+				  unsigned int type);
+hfilter create_chebychev_filter(double fc, unsigned int num_pole,
+                                unsigned int nchann, int highpass,
+				double ripple, unsigned int type);
 hfilter create_integrate_filter(unsigned int nchann, unsigned int type);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /*COMMON_FILTERS_H*/
--- src/filter.h
+++ src/filter.h
@@ -1,18 +1,48 @@
+/*
+      Copyright (C) 2008-2009 Nicolas Bourdaud <nicolas.bourdaud@epfl.ch>
+
+    This file is part of the rtfilter library
+
+    The eegpanel library is free software: you can redistribute it and/or
+    modify it under the terms of the version 3 of the GNU General Public
+    License as published by the Free Software Foundation.
+  
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #ifndef FILTER_H
 #define FILTER_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 #define DATATYPE_FLOAT	0
 #define DATATYPE_DOUBLE	1
 
 typedef const struct _dfilter* hfilter;
 
-hfilter create_filter_f(unsigned int nchann, unsigned int alen, const float *a, unsigned int blen, const float *b, unsigned int type);
-hfilter create_filter_d(unsigned int nchann, unsigned int alen, const double *a, unsigned int blen, const double *b, unsigned int type);
-void filter_f(hfilter filt, const float* x, float* y, unsigned int num_samples);
-void filter_d(hfilter filt, const double* x, double* y, unsigned int num_samples);
+hfilter create_filter_f(unsigned int nchann, 
+                        unsigned int alen, const float *a,
+			unsigned int blen, const float *b,
+			unsigned int type);
+hfilter create_filter_d(unsigned int nchann,
+                        unsigned int alen, const double *a,
+			unsigned int blen, const double *b,
+			unsigned int type);
+void filter_f(hfilter filt, const float* x, float* y, unsigned int ns);
+void filter_d(hfilter filt, const double* x, double* y, unsigned int ns);
 void reset_filter(hfilter filt);
 void init_filter(hfilter filt, const void* val);
 void destroy_filter(hfilter filt);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif //FILTER_H
