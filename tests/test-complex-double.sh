c3b199d93a6a5769e1de066daabfa8eb27c79c58
Nicolas Bourdaud <nicolas.bourdaud@epfl.ch>
2010-12-03 13:01:09 +0100

    Add more test for compplex filter

--- tests/test-complex-double.sh
+++ tests/test-complex-double.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 cmpwmatlab* -d 1 -p 3 -c 15 > test.log
+cmpwmatlab* -d 1 -p 3 -c 16 > test.log
 
 cmpwmatlab* -d 3 -p 1 -c 15 > test.log
+cmpwmatlab* -d 3 -p 1 -c 16 > test.log
 
 cmpwmatlab* -d 3 -p 3 -c 15 > test.log
+cmpwmatlab* -d 3 -p 3 -c 16 > test.log
 
 rm test.log
--- tests/test-complex-float.sh
+++ tests/test-complex-float.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 cmpwmatlab* -d 0 -p 2 -c 15 > test.log
+cmpwmatlab* -d 0 -p 2 -c 16 > test.log
 
+cmpwmatlab* -d 2 -p 0 -c 16 > test.log
 cmpwmatlab* -d 2 -p 0 -c 15 > test.log
 
+cmpwmatlab* -d 2 -p 2 -c 16 > test.log
 cmpwmatlab* -d 2 -p 2 -c 15 > test.log
 
 rm test.log
