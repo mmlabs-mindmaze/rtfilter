2d7e868540554e60b4b8818d6920c9a2f22dd537
Nicolas Bourdaud <nicolas.bourdaud@epfl.ch>
2011-08-05 15:53:15 +0200

    Relicense the library into LGPL3

--- src/filter.c
+++ src/filter.c
@@ -1,18 +1,18 @@
 /*
-      Copyright (C) 2008-2010 Nicolas Bourdaud <nicolas.bourdaud@epfl.ch>
+    Copyright (C) 2008-2011 Nicolas Bourdaud <nicolas.bourdaud@epfl.ch>
 
     This file is part of the rtfilter library
 
     The rtfilter library is free software: you can redistribute it and/or
-    modify it under the terms of the version 3 of the GNU General Public
-    License as published by the Free Software Foundation.
+    modify it under the terms of the version 3 of the GNU Lesser General
+    Public License as published by the Free Software Foundation.
   
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+    GNU Lesser General Public License for more details.
     
-    You should have received a copy of the GNU General Public License
+    You should have received a copy of the GNU Lesser General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #if HAVE_CONFIG_H
