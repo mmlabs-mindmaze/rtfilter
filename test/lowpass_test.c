8a1dfde9151604a2f2e415e76ce36295e78245f6
Gabriel Ganne <gabriel.ganne@mindmaze.com>
2020-01-15 15:49:43 +0100

    add mindmaze copyright where needed
    remove occurences of @mindmaze_header@ pattern which is not replaced
    during call to dist target in this project
    
    Change-Id: I915eb5bf9927da0e5c37eca28f4eda4353dccc7a

--- test/lowpass_test.c
+++ test/lowpass_test.c
@@ -1,6 +1,21 @@
 /*
- * @mindmaze_header@
+ *  Copyright (C) 2018-2020 Mindmaze Holdings SA
+ *
+ *  This file is part of the rtfilter library
+ *
+ *  The rtfilter library is free software: you can redistribute it and/or
+ *  modify it under the terms of the version 3 of the GNU Lesser General
+ *  Public License as published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 #include <check.h>
 #include <math.h>
 #include <rtfilter.h>
